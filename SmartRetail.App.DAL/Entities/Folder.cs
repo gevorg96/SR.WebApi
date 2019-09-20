8fb73199124e8cc38b162dcc6300fea135862950
gkesyan <gevorgkesyan@gmail.com>
2019-09-20 16:57:52 +0300

    1

--- SmartRetail.App.DAL/Entities/Folder.cs
+++ SmartRetail.App.DAL/Entities/Folder.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Dapper.Contrib.Extensions;
 
 namespace SmartRetail.App.DAL.Entities
 {
     public class Folder: IEntity
     {
+        [Key]
         public int id { get; set; }
         public int business_id { get; set; }
         public int? parent_id { get; set; }
@@ -19,5 +21,10 @@ namespace SmartRetail.App.DAL.Entities
             else
                 throw new Exception("Используется объект не того типа.");
         }
+
+        [Write(false)]
+        [Computed]
+        public virtual Product Product { get; set; }
+        
     }
 }
