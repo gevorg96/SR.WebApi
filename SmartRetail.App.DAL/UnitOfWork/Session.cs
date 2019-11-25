08b2e7e65558fc3f1eb516782708da4af5ad5b44
gkesyan <gevorgkesyan@gmail.com>
2019-11-25 19:12:59 +0300

    postgres

--- SmartRetail.App.DAL/UnitOfWork/Session.cs
+++ SmartRetail.App.DAL/UnitOfWork/Session.cs
@@ -13,7 +13,7 @@ namespace SmartRetail.App.DAL.UnitOfWork
 
         public Session()
         {
-            _connection = new SqlConnection(@"Data Source=SQL6007.site4now.net;Initial Catalog=DB_A4E50E_smartretail;User Id=DB_A4E50E_smartretail_admin;Password=1234QWer;");
+            _connection = new SqlConnection(@"database=d369fmctn65rq9;host=ec2-46-137-91-216.eu-west-1.compute.amazonaws.com;password=f8774cccfae05a0a4c4bd86c0d40d504db71cd50ebaa52d5ad6767709ffcd5d4;username=nfedurzdslzhpo;port=5432;pooling=True;trust server certificate=True;ssl mode=Require");
             _connection.Open();
             UnitOfWork = new UnitOfWork(_connection);
         }
